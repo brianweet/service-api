99944b3f91ad5e30360bc59d1e3ae444f4bc9d68
Māris Krivtežs <marisks@gmail.com>
2016-06-22 13:47:36 +0300

    Added documentation to models.

--- src/Geta.ServiceApi.Commerce/Models/Address.cs
+++ src/Geta.ServiceApi.Commerce/Models/Address.cs
@@ -2,42 +2,99 @@
 
 namespace Geta.ServiceApi.Commerce.Models
 {
+    /// <summary>
+    /// Address model.
+    /// </summary>
     public class Address
     {
+        /// <summary>
+        /// Address ID (GUID).
+        /// </summary>
         public Guid? AddressId { get; set; }
 
+        /// <summary>
+        /// Last modified date.
+        /// </summary>
         public DateTime? Modified { get; set; }
 
+        /// <summary>
+        /// Address name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// First name.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last name.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Country name.
+        /// </summary>
         public string CountryName { get; set; }
 
+        /// <summary>
+        /// Country code.
+        /// </summary>
         public string CountryCode { get; set; }
 
+        /// <summary>
+        /// City.
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// Postal code (Zip code).
+        /// </summary>
         public string PostalCode { get; set; }
 
+        /// <summary>
+        /// Address line 1.
+        /// </summary>
         public string Line1 { get; set; }
 
+        /// <summary>
+        /// Address line 2.
+        /// </summary>
         public string Line2 { get; set; }
 
+        /// <summary>
+        /// Region.
+        /// </summary>
         public string Region { get; set; }
 
+        /// <summary>
+        /// Email.
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Check if this is default shipping address.
+        /// </summary>
         public bool ShippingDefault { get; set; }
 
+        /// <summary>
+        /// Check if this is default billing address.
+        /// </summary>
         public bool BillingDefault { get; set; }
 
+        /// <summary>
+        /// Daytime phone number.
+        /// </summary>
         public string DaytimePhoneNumber { get; set; }
 
+        /// <summary>
+        /// Evening phone number.
+        /// </summary>
         public string EveningPhoneNumber { get; set; }
 
+        /// <summary>
+        /// Organization name.
+        /// </summary>
         public string Organization { get; set; }
     }
 }
