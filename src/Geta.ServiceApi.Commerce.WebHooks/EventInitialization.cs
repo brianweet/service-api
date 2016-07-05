af1ed7bc195b589094a357535bfa80a9a082604d
Māris Krivtežs <marisks@gmail.com>
2016-07-05 17:44:49 +0300

    Added initial event initialization module,.

---
+++ src/Geta.ServiceApi.Commerce.WebHooks/EventInitialization.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace Geta.ServiceApi.Commerce.WebHooks
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class EventInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
