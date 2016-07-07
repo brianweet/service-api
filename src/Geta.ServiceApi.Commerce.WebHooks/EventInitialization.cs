5b6ab1a26a5acd17804c1e51050be23093ab6650
Māris Krivtežs <marisks@gmail.com>
2016-07-07 10:25:52 +0300

    #10 Added event EPiServer event handlers.

--- src/Geta.ServiceApi.Commerce.WebHooks/EventInitialization.cs
+++ src/Geta.ServiceApi.Commerce.WebHooks/EventInitialization.cs
@@ -1,5 +1,8 @@
 ﻿using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using Mediachase.Commerce.Engine.Events;
+using Mediachase.Commerce.Orders;
 
 namespace Geta.ServiceApi.Commerce.WebHooks
 {
@@ -7,11 +10,38 @@ namespace Geta.ServiceApi.Commerce.WebHooks
     [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
     public class EventInitialization : IInitializableModule
     {
+        public CatalogKeyEventBroadcaster EventBroadcaster => ServiceLocator.Current.GetInstance<CatalogKeyEventBroadcaster>();
+
         public void Initialize(InitializationEngine context)
         {
+            EventBroadcaster.InventoryUpdated += OnInventoryUpdated;
+            EventBroadcaster.PriceUpdated += OnPriceUpdated;
+
+            OrderContext.Current.OrderGroupUpdated += OnOrderGroupUpdated;
+            OrderContext.Current.OrderGroupDeleted += OnOrderGroupDeleted;
         }
 
         public void Uninitialize(InitializationEngine context)
+        {
+            EventBroadcaster.InventoryUpdated -= OnInventoryUpdated;
+            EventBroadcaster.PriceUpdated -= OnPriceUpdated;
+            OrderContext.Current.OrderGroupUpdated -= OnOrderGroupUpdated;
+            OrderContext.Current.OrderGroupDeleted -= OnOrderGroupDeleted;
+        }
+
+        private void OnOrderGroupDeleted(object sender, OrderGroupEventArgs e)
+        {
+        }
+
+        private void OnOrderGroupUpdated(object sender, OrderGroupEventArgs orderGroupEventArgs)
+        {
+        }
+
+        private void OnPriceUpdated(object sender, PriceUpdateEventArgs e)
+        {
+        }
+
+        private void OnInventoryUpdated(object sender, InventoryUpdateEventArgs e)
         {
         }
     }
