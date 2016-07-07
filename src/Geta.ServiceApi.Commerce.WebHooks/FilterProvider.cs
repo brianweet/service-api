850ec7dd969c077f8c693336073a55c47b90464e
Māris Krivtežs <marisks@gmail.com>
2016-07-07 10:38:25 +0300

    #10 Added WebHook filter provider.

---
+++ src/Geta.ServiceApi.Commerce.WebHooks/FilterProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+
+namespace Geta.ServiceApi.Commerce.WebHooks
+{
+    public class FilterProvider : IWebHookFilterProvider
+    {
+        private readonly Collection<WebHookFilter> _filters = new Collection<WebHookFilter>
+        {
+            new WebHookFilter { Name = "InventoryUpdated", Description = "Event raised when an inventory has been updated."},
+            new WebHookFilter { Name = "PriceUpdated", Description = "Event raised when one or many prices have been updated."},
+            new WebHookFilter { Name = "OrderGroupUpdated", Description = "Event raised when order group updated."},
+            new WebHookFilter { Name = "OrderGroupDeleted", Description = "Event raised when order group deleted."},
+        };
+
+        public Task<Collection<WebHookFilter>> GetFiltersAsync()
+        {
+            return Task.FromResult(_filters);
+        }
+    }
+}
